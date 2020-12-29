5c6292a8763aa90d7801e70b732c4853820b86ce
calmisential <xinyu_std@163.com>
2020-12-29 19:42:51 +0800

    vgg for backbone

---
+++ openpose/core/backbone.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+
+class VGG(tf.keras.layers.Layer):
+    def __init__(self):
+        super(VGG, self).__init__()
+        pass
+
+    def call(self, inputs, **kwargs):
+        pass
--- openpose/core/model.py
+++ openpose/core/model.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 
 
+
 class CPM(tf.keras.Model):
     def __init__(self):
         super(CPM, self).__init__()
