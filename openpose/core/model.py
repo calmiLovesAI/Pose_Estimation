93b8b3c3c3676bdfa6850ddb681208b5e88fb898
calmisential <xinyu_std@163.com>
2020-12-29 19:34:49 +0800

    Update

---
+++ openpose/core/model.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+
+class CPM(tf.keras.Model):
+    def __init__(self):
+        super(CPM, self).__init__()
+        pass
+
+    def call(self, inputs, training=None, mask=None):
+        pass
