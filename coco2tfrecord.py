4661f490cddf1d47dde400625756e56bd9410e5f
calmisential <xinyu_std@163.com>
2021-03-09 21:07:28 +0800

    Update

---
+++ coco2tfrecord.py
@@ -0,0 +1,142 @@
+import tensorflow as tf
+import numpy as np
+import cv2
+
+from pycocotools.coco import COCO
+from configuration import OpenPoseCfg
+from openpose.data.tfrecord_utils import FileSharder, encode_example
+
+
+
+def reshape_kpts(keypoints):
+    kpts_np = np.array(keypoints, dtype=np.float32)
+    kpts_np = np.reshape(kpts_np, (OpenPoseCfg.dataset_num_keypoints, 3))
+    return kpts_np
+
+
+def map_new_kpts(keypoints):
+    new_kpts = []
+    for kpt_name, kpt_def in OpenPoseCfg.KEYPOINTS_DEF.items():
+        ds_idxs = kpt_def["ds_idxs"]
+        assert type(ds_idxs) is int or (type(ds_idxs) is tuple and len(ds_idxs) == 2)
+
+        if type(ds_idxs) is tuple:
+            first_kpt = keypoints[ds_idxs[0]]
+            second_kpt = keypoints[ds_idxs[1]]
+            new_kpt = np.array(middle_kpt(first_kpt, second_kpt), dtype=np.float32)
+        else:
+            new_kpt = keypoints[ds_idxs]
+        new_kpts.append(new_kpt)
+    return new_kpts
+
+
+def middle_kpt(kpt1, kpt2):
+    if kpt1[2] == 0 or kpt2[2] == 0:
+        return 0, 0, 0
+    else:
+        return [
+                (kpt1[0] + kpt2[0]) / 2,
+                (kpt1[1] + kpt2[1]) / 2,
+                min(kpt1[2], kpt2[2])
+                ]
+
+
+def transform_keypts(keypoints, size):
+    X = np.array(keypoints[..., 0], dtype=np.float32)
+    Y = np.array(keypoints[..., 1], dtype=np.float32)
+    keypoints[..., 0] = Y
+    keypoints[..., 1] = X
+
+    keypoints[:, :, 0:2] = keypoints[:, :, 0:2] / size
+    return keypoints
+
+
+def create_all_joints(all_keypts):
+    """create a joints tensor from keypoints tensor, according to COCO joints
+    :param config: effective config dict, must include JOINTS_DEF
+    :param all_keypts - tensor of shape (number of persons,number of kpts(DS_NUM_KEYPOINTS),3)
+    :return tensor of shape (number of persons,number of joints(19),5)"""
+
+    def create_joints(keypts):
+        joints = []
+        for joint_name, joint_def in OpenPoseCfg.JOINTS_DEF.items():
+            kp1_name, kp2_name = joint_def["kpts"]
+            kp1_idx = OpenPoseCfg.KEYPOINTS_DEF[kp1_name]["idx"]
+            kp2_idx = OpenPoseCfg.KEYPOINTS_DEF[kp2_name]["idx"]
+            kp1 = keypts[kp1_idx]
+            kp2 = keypts[kp2_idx]
+            if kp1[2] == 0 or kp2[2] == 0:
+                # if either of the keypoints is missing, the joint is zero
+                new_joint = (0, 0, 0, 0, 0)
+                joints.append(new_joint)
+                continue
+            # create new joint from both keypoint coords, with the visibility being the minimum of either keypoint
+            new_joint = (*kp1[0:2], *kp2[0:2], min(kp1[2], kp2[2]))
+            joints.append(new_joint)
+        return np.array(joints, dtype=np.float32)
+
+    all_joints = [create_joints(x) for x in all_keypts]  # for each person
+
+    # numpify result transpose joints
+    return np.array(all_joints, dtype=np.float32).transpose((1, 0, 2))
+
+
+if __name__ == '__main__':
+    annotation_file = OpenPoseCfg.train_anns
+    print("开始处理："+annotation_file)
+    coco = COCO(annotation_file)
+    category = 1
+    imgIds = coco.getImgIds(catIds=[category])
+    imgIds.sort()
+
+    print("选取的图片数量：", len(imgIds))
+
+    filename_format = OpenPoseCfg.train_tfrecords + "-{:03}.tfrecords"
+    with FileSharder(tf.io.TFRecordWriter, filename_format, OpenPoseCfg.images_per_tfrecord) as writer:
+        for img_id in imgIds:
+            image_info = coco.loadImgs(ids=img_id)[0]
+            size = [image_info['height'], image_info['width']]
+
+            annIds = coco.getAnnIds(imgIds=[img_id])
+            anns = coco.loadAnns(ids=annIds)
+
+            person_keypoints = []
+            for annotation in anns:
+                if annotation['num_keypoints'] > 0:
+                    kpts = annotation['keypoints']
+                    kpts = reshape_kpts(kpts)
+                    kpts = map_new_kpts(kpts)
+
+                    person_keypoints.append(kpts)
+            if not person_keypoints:
+                continue
+
+            person_keypoints = np.array(person_keypoints, dtype=np.float32)
+            keypoints = transform_keypts(person_keypoints, np.array(size, dtype=np.int))
+            tr_joint = create_all_joints(keypoints)
+            tr_keypoints = tr_joint.transpose((1, 0, 2))
+
+            total_mask = np.zeros(size, dtype=np.float32)
+
+            for annotation in anns:
+                if annotation['num_keypoints'] == 0:
+                    single_mask = coco.annToMask(annotation)
+                    total_mask = np.max([single_mask, total_mask], axis=0)
+
+            total_mask = cv2.resize(total_mask, dsize=OpenPoseCfg.model_output_size)
+            total_mask = (total_mask > 0.01).astype(np.int16)
+
+            kernel = np.ones((5, 5), np.uint8)
+            total_mask = cv2.dilate(total_mask, kernel)
+            total_mask = total_mask.astype(np.bool)
+            total_mask = np.invert(total_mask)
+            total_mask = total_mask.astype(np.float32)
+
+            try:
+                img_path = OpenPoseCfg.coco_images + "/" + image_info['file_name']
+                image_raw = tf.io.read_file(img_path)
+            except:
+                print("无法读取文件：%s" % img_path)
+                continue
+            example = encode_example(img_id, image_raw, size, tr_keypoints, tr_joint, total_mask)
+            writer.write(example)
---
+++ openpose/data/tfrecord_utils.py
@@ -0,0 +1,74 @@
+import tensorflow as tf
+
+
+def int64_feature(value):
+    if type(value) != list:
+        value = [value]
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
+
+
+def bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+def encode_example(image_id, image_raw, size, kpts, joints, mask):
+    kpts = tf.constant(kpts)
+    kpts = tf.io.serialize_tensor(kpts).numpy()
+    joints = tf.constant(joints)
+    joints = tf.io.serialize_tensor(joints).numpy()
+    mask = tf.constant(mask)
+    mask = tf.io.serialize_tensor(mask).numpy()
+
+    feature = {
+        'id': int64_feature(image_id),
+        'image_raw': bytes_feature(image_raw.numpy()),
+        'size': int64_feature(size),
+        'kpts': bytes_feature(kpts),
+        'joints': bytes_feature(joints),
+        'mask': bytes_feature(mask)
+    }
+
+    example_proto = tf.train.Example(features=tf.train.Features(feature=feature))
+    return example_proto.SerializeToString()
+
+
+
+class FileSharder:
+    def __init__(self, file_writer, base_filename_format: str, records_per_file: int, verbose: bool = True):
+        assert base_filename_format.format(0) != base_filename_format
+
+        self._file_writer = file_writer
+        self._base_filename_format = base_filename_format
+        self._records_per_file = records_per_file
+        self._example_counter = 0
+        self._file_counter = 1
+        self._verbose = verbose
+        self._start_file()
+
+    def __enter__(self):
+        return self
+
+    def _start_file(self):
+        self._filename = self._base_filename_format.format(self._file_counter)
+        if self._verbose: print("\nWriting file:" + self._filename, flush=True)
+        self._writer = self._file_writer(self._filename)
+
+    def _finish_file(self):
+        self._writer.flush()
+        self._writer.close()
+
+    def _advance_file(self):
+        self._finish_file()
+        self._file_counter += 1
+        self._example_counter = 0
+        self._start_file()
+
+    def write(self, item):
+        self._writer.write(item)
+        if self._verbose and not self._example_counter % 100: print(".", end="", flush=True)
+        self._example_counter += 1
+        if not self._example_counter % self._records_per_file:
+            self._advance_file()
+
+    def __exit__(self, *args):
+        self._finish_file()
